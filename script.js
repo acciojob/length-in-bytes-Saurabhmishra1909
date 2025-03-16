19db9026a9f3a8d988a861ca25131ef8d529d7d3
Saurabh Mishra <84716420+Saurabhmishra1909@users.noreply.github.com>
2025-03-16 23:15:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const byteSize = (str) => {
-  // write your code hedre
-	return str.length;
-	
+  // write your code hedr
+	const encoder = new TextEncoder();
+
+  // Encode the string into a Uint8Array (UTF-8 bytes)
+  const bytes = encoder.encode(str);
+
+  // Return the length of the byte array
+  return bytes.length;
 };
 
 // Do not change the code below
